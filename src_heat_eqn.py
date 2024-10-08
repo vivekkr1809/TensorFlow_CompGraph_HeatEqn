e37ab51ff3cf98e025bc9b2030b1f2ce7edc4328
Vivek <vivekkr1809@gmail.com>
2024-10-07 21:17:43 -0700

    initial commit

--- src_heat_eqn.py
+++ src_heat_eqn.py
@@ -3,9 +3,20 @@ This is the main source file to solve the heat equation.
 
 Author(s): Vivek Kumar
 """
+from geometry.rectangle import RectangularMesh
+
+def generate_mesh(dim, ):
+    """
+    Generate the mesh for the problem
+    """
+    return 0
+    
 
 
 def main():
+
+    mesh = RectangularMesh(dim = 2, length = 1.0, width = 1.0, nelements = 100, method = 'delaunay')
+
     return 0
 
 
